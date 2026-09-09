8f30ae0ef47a8ca56d6ff1b94df27a7dc57e2298
YIAHIAAHMED <yiahiaahmed@gmail.com>
2026-09-09 08:05:20 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { Suspense, useState } from 'react';
 import './App.css'
 import Banner from './components/homepage/banner/Banner'
--- src/components/homepage/players/selectedPlayers/SelectedPlayers.jsx
+++ src/components/homepage/players/selectedPlayers/SelectedPlayers.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FaUser } from 'react-icons/fa';
-import { MdDelete } from 'react-icons/md';
+
+import SelectedCard from '../../../ui/SelectedCard';
 
 const SelectedPlayers = ({ 
     selectedPlayers, 
@@ -31,25 +31,7 @@ const SelectedPlayers = ({
                     
                      : selectedPlayers.map((player, index) => {
                         return (
-                            <div key={index} className='flex items-center gap-6 justify-between p-10 rounded-2xl border' >
-                                <div className='flex items-center gap-6'>
-                                    <img
-                                        src={player.image}
-                                        alt={player.name}
-                                        className='h-[75px] w-auto rounded-md ' />
-                                    <div>
-                                        <h2 className='flex items-center gap-2 font-semibold text-xl'><FaUser /> {player.name}</h2>
-                                        <p>{player.role} </p>
-                                    </div>
-
-                                </div>
-                                <button
-                                    className='btn text-red-500'
-                                    onClick={() => handleDeleteSelectedPlayer(player) }
-                                ><MdDelete />
-                                </button>
-
-                            </div>
+                            <SelectedCard player={player} handleDeleteSelectedPlayer={handleDeleteSelectedPlayer} key={index} ></SelectedCard>
                         );
                     })
                 }
